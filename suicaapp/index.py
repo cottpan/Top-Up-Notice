6e3931d6c7ad08181220e67fe1f8dede6473b654
入江政行 <marky080120@gmail.com>
2017-09-16 22:13:47 +0900

    api改善

--- suicaapp/index.py
+++ suicaapp/index.py
@@ -1,7 +1,7 @@
 from bottle import route, run, template
 
-@route('/api/user/<id:int>')
-def index(id):
-    return template('<b>残高が {{id}}円です。JR線改札には入れません。チャージしてください。</b>', id=id)
+@route('/api/user/<id:int>/<balance:int>')
+def index(id,balance):
+    return template('<b>ユーザ{{id}}様</b></br></br><b>残高が {{balance}}円です。JR線改札には入れません。チャージしてください。</b>', id=id , balance=balance)
 
 run(host='localhost', port=8080)
