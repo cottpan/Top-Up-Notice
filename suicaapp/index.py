c4f551cf9b433c02d83ecd032577e97526cce6e5
isoisokaz <cs22m-isogaya@stu.kobe-u.ac.jp>
2017-09-17 01:12:21 +0900

    サーバー(一応動く)

--- suicaapp/index.py
+++ suicaapp/index.py
@@ -1,16 +1,19 @@
 from bottle import route, run, template
 import cv2
 
-@route('/api/test1', method='GET')
+@route('/api/test1')
 def ring_app():
     img = cv2.imread("./test1.png")
-    cv2.imshow("Test", img)
+    cv2.imshow("Test1", img)
     cv2.waitKey(0)
-
-@route('/api/test2', method='GET')
-def ring_app():
-    cv2.img = imread("./test2.png")
-    cv2.imshow("Test", img)
+    cv2.destroyAllWindows()
+    
+@route('/api/test2')
+def ring_app2():
+    img = cv2.imread("./test2.png")
+    cv2.imshow("Test2", img)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-run(host='localhost', port=8080)
+run(host='0.0.0.0', port=8080)
+ 
